4d03d39ab6e9b9c53b5a75e0d201e81943fd1132
Josue Cesar <mendonca.josue@gmail.com>
2020-03-08 17:29:39 -0300

    criação e autenticação de usuários

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ceb5c40cd23d3dbfa43b08187f5bf85d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/authMiddleware';
+
 const routes = new Router();
+
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/user', UserController.store);
 
 export default routes;
